c8afbf4c2c97b77582d80fb3aaa4816ad45f841e
Сергей Бризгалов <s.v.brizgalov@yandex.ru>
2023-04-20 22:54:50 +0500

    add working version without classes

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """Атака."""
     if char_class == 'warrior':
         return (
             f'{char_name} нанёс урон противнику равный {5 + randint(3, 5)}')
@@ -17,6 +18,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Применение защиты."""
     if char_class == 'warrior':
         return f'{char_name} блокировал {10 + randint(5, 10)} урона'
     if char_class == 'mage':
@@ -28,6 +30,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Применение специальных навыков."""
     if char_class == 'warrior':
         return (
             f'{char_name}'
@@ -41,6 +44,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Управление навыками персонажа."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -68,6 +72,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Выбираем класс персонажа."""
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
